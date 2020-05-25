d796d06609b8ee5dae8157c3eb54c54bfc53cee5
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2020-05-25 16:12:31 -0300

    Commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:'https://prismatoolkit-backend.herokuapp.com/',
+    baseURL:'https://prismatoolkit-backend.herokuapp.com',
 })
 
 export default api;
